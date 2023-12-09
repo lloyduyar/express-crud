0a32c3a779a7f8202a4db06379faf0d26241a8d4
Lloyd Urey <47035968+lilnelly355@users.noreply.github.com>
2023-12-09 14:02:25 -0100

    Update routing.js

--- routing.js
+++ routing.js
@@ -35,7 +35,6 @@ app.get("/getEntries", (req, res) => {
     if (results.length != 0) {
       res.json(results);
       console.log(results);
-      res.redirect("/");
       console.log("There are/is " + results.length + " database entries!");
     } else {
       res.json([]);
